afa8969c6a194ee52d09dfb6b30df50092f0aaa0
nikaNikesh <example@gmail.com>
2023-07-12 17:11:48 +0300

    master: add oop

--- leetcode/easy/dataStructure/string/countPrefixes.js
+++ leetcode/easy/dataStructure/string/countPrefixes.js
@@ -1,13 +1,63 @@
+/*2255. Count Prefixes of a Given String
+
+You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+
+Return the number of strings in words that are a prefix of s.
+
+A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+
+Input: words = ["a","b","c","ab","bc","abc"], s = "abc"
+Output: 3*/
+
 /**
  * @param {string[]} words
  * @param {string} s
  * @return {number}
  */
-let countPrefixes = function(words, s) {
+/*let countPrefixes = function(words, s) {
+    let counter = 0;
+    let prefix = '';
+    for (let i = 0; i < s.length; i++) {
+        prefix += s[i];
+        for (let j = 0; j < words.length; j++) {
+            if (words[j] === prefix) {
+                counter++;
+            }
+        }
+    }
 
+return counter;
 };
+console.log(countPrefixes(['r', 'm', 'be', 'b', 'br', 'ber', 'e'], 'ber'));*/
 
-let isPrefixes = function(prefix, s) {
+let isPrefix = function (prefix, s) {
+    if (s.startsWith(prefix)) {
+        return true;
+    }
+    return false;
+}
 
 
+let countPrefixes = function (word, s) {
+    let counter = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (isPrefix(word[i], s)) {
+            counter++;
+        }
+    }
+    return counter;
 };
+console.log(countPrefixes(['r', 'm', 'be', 'b', 'br', 'ber', 'e'], 'ber'));
+
+//LC
+/*var countPrefixes = function(words, s) {
+     let counter = 0
+     for (let i = 0; i < words.length; i++) {
+         if (words[i] === s.slice(0,words[i].length)) {
+            counter++
+        }
+     }
+    return counter
+};*/
--- theory/methodsBIObject.js
+++ theory/methodsBIObject.js
@@ -14,14 +14,34 @@ let num = 3.14159;
 let roundedNum = num.toFixed(2);
 console.log('Округляет числа до указанного количества знаков после запятой и возвращает строку: ' + roundedNum);
 
-let num2 = 42;
-console.log('Возвращает число как строку, можно с указанием с/с: ' + num.toString(2));
+let num2 = 1024;
+console.log('Возвращает число как строку, можно с указанием с/с: ' + num2.toString(2));
 
 
-let str = 'cat'
+let str = 'crazy'
 console.log('Возвращает символ по указанному индексу в строке: ' + str.charAt(1));
 console.log('Объединяет две или более строк и возвращает новую строку: ' + str.concat('dog'));
 console.log('Возвращает индекс первого вхождения указанной подстроки в строку : ' + str.indexOf('at'));
+console.log('Возвращает строку в верхнем регистре : ' + str.toUpperCase());
+let str2 = 'CRAZY'
+st
+console.log('Возвращает сроку в нижнем регистре : ' + str2.toLowerCase());
+let str3 = ' apple   '
+console.log('Возвращает новую строку без пробелов в начале и конце : ' + str3.trim());
+
+
+let bigNumber = 123939930275656474883229984477373778282229239393932922722737n;
+console.log('Возвращает строку с разделением на разряды : ' + bigNumber.toLocaleString());
+
+let newBoolean = Boolean(false);
+console.log('Возвращает примитив того же значения объекта: ' + newBoolean.valueOf());
+
+const symbol = Symbol('описание');
+console.log('Возвращает строковое представление символа: ' + symbol.toString());
+
+
+
+
 
 
 
